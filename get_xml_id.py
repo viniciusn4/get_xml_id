0aba01f782552749a331f9d1bfd5944d3064a8e7
Vinicius Nogueira Costa <vinicius_n4@hotmail.com>
2022-05-08 23:43:33 -0300

    feature: allow to read any label

--- get_xml_id.py
+++ get_xml_id.py
@@ -24,57 +24,57 @@ except:
         data = xml.read()
 
 bs_data = BeautifulSoup(data, 'xml')
-batch_id = bs_data.find('id')
-batch_name = bs_data.find('name')
-live_tag = bs_data.find_all('tag', {'label': 'live'})
-spoof_tag = bs_data.find_all('tag', {'label': 'spoof'})
-undefined_tag = bs_data.find_all('tag', {'label': 'undefined'})
+batch_id = bs_data.find('id').get_text()
+batch_name = bs_data.find('name').get_text()
+# live_tag = bs_data.find_all('tag', {'label': 'live'})
+# spoof_tag = bs_data.find_all('tag', {'label': 'spoof'})
+# undefined_tag = bs_data.find_all('tag', {'label': 'undefined'})
 all_image = bs_data.find_all('image')
 all_tag = bs_data.find_all('tag')
 
-print(f'>>> Batch ID: {batch_id.get_text()}')
-print(f'>>> Batch name: {batch_name.get_text()}')  # Nome do lote do CVAT que está sendo lido.
+# Fazendo o script funcionar independente da label que vier.
+find_labels = bs_data.find('labels')
+labels_name = find_labels.find_all('name')
+labels_list = [label.get_text() for label in labels_name]
 
 
-def save_content(filename, content, mode):
+def save_content(labels, batch_id):
     """Save the content into a file.
 
     Parameters
     -----------
-    filename : string
-        Path to the file where the content is saved.
-    content : array
-        List containing ids.
-    mode : string
-        Type of annotation (live | spoof | undefined)
+    labels : list
+        List containing all the possibly labels in the XML file.
+    batch_id : int
+        Number ID of the batch.
     """
-    sorted_list = []
-    with open(f'{filename}.txt', 'w') as txt:
-        for tag in content:
-            get_index = tag.parent.get('id')
-            sorted_list.append(int(get_index))
-            repeated_label.append(get_index)
-        sorted_list.sort()
-        for index in sorted_list:
-            txt.write(f'{mode} - {index}\n')
+    for label in labels:
+        sorted_list = []
+        with open(f'{batch_id}_{label}.txt', 'w') as txt:
+            content = bs_data.find_all('tag', {'label': label})
+            for tag in content:
+                get_index = tag.parent.get('id')
+                sorted_list.append(int(get_index))
+                repeated_label.append(get_index)
+            sorted_list.sort()
+            for index in sorted_list:
+                txt.write(f'{label} - {index}\n')
+            print(f'There are {len(sorted_list)} "{label}" images in this batch.')
 
 
-save_content('live_id', live_tag, 'live')
-print(f'\nThere are {len(live_tag)} live images in this batch.')
+print(f'>>> Batch ID: {batch_id}')
+print(f'>>> Batch name: {batch_name}\n')  # Nome do lote do CVAT que está sendo lido.
 
-save_content('spoof_id', spoof_tag, 'spoof')
-print(f'There are {len(spoof_tag)} spoof images in this batch.')
-
-save_content('undefined_id', undefined_tag, 'undefined')
-print(f'There are {len(undefined_tag)} undefined images in this batch.')
+save_content(labels_list, batch_id)
 
 # Repeated labels:
 for i in repeated_label:
     if repeated_label.count(i) > 1:
-        repeated_list.append(i)
+        repeated_list.append(int(i))
         while repeated_label.count(i) > 1:  # Esse trecho é porque estava printando duplicado os casos repetidos.
             index = repeated_label.index(i)  # Acredito que dê para usar set() pra excluir os duplicados.
             repeated_label.pop(index)
+repeated_list.sort()
 print(f'\nThere are {len(repeated_list)} repeated labels: {repeated_list}')
 
 # Empty labels:
@@ -83,7 +83,7 @@ for tag in all_tag:
 for image in all_image:
     image_id = image.get('id')
     if image_id not in all_tag_id:  # Comparo uma list com todos IDs de <image> contra apenas os anotados <tag>.
-        empty_tag.append(image_id)
+        empty_tag.append(int(image_id))
 empty_tag.sort()
 print(f'There are {len(empty_tag)} images without label: {empty_tag}\n')
 
